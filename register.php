2eeb18d95ced34fa3c33d9b481d42941ce7b56b1
Johanna <jmarie.dino@gmail.com>
2024-05-20 00:56:53 +0800

    Update register.php

--- register.php
+++ register.php
@@ -150,12 +150,15 @@ $limitStart = ($currentPage - 1) * $rowsPerPage;
 
 
                 <div>
+                
+
+                <!--Admin Dropdown-->
+                <div class="max-w-lg">
+
                 <h1 class="text-orange-950 text-4xl font-bold my-auto">
                   Register
                 </h1>
 
-                <!--Admin Dropdown-->
-                <div class="max-w-lg">
                 <button
                   class="text-[#424242] bg-transparent hover:bg-transparent focus:ring-4 focus:ring-transparent font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
                   type="button"
